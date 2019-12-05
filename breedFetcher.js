b60730dc1725d59555afe338af30535ab1741209
Panos Hatzinikolaou <pjhatzis@gmail.com>
2019-12-05 23:58:09 +0000

    initial commit, user now will use the index.js file to run the program and find the kitty description

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,17 +1,27 @@
-const request = require('request')
+const request = require('request');
 
 
 // querying dynamically with command line arguments, with the user entering
 // only the name of the breed they want to query.
-let breeds = process.argv.slice(2);
-let breed = breeds[0];
-request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, function(error, response, body){
-  // console.log(`Body: ${body}`);
-  // console.log(`type of body is ${typeof body}`);
-  if (error){
-    console.log(error, "Breed not found. Maybe you were looking for a dog?")
-  }
-  const data = JSON.parse(body);
-  console.log(data);
-  console.log(`type of body after parsing from JSON is ${typeof data}`);
-})
+
+
+const fetchBreedDescription = function(breed, callback) {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, function(error, response, body) {
+    if (error) {
+      callback(error, null);
+      return null;
+    }
+    const data = JSON.parse(body);
+
+    if (data.length === 0) {
+      callback('breed not found');
+      return null;
+    }
+    callback(null, data[0].description);
+  });
+};
+
+module.exports = { fetchBreedDescription };
+
+// let breeds = process.argv.slice(2);
+// let breed = breeds[0];
